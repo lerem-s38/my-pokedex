88e233c9c69ebb25bbd795f937e8dc4dad867ec0
Rémi Cusset <56927808+lerem-s38@users.noreply.github.com>
2020-12-02 20:58:45 +0100

    Environnement

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, { FunctionComponent } from 'react';
+
+const App: FunctionComponent = () => {
+  const name: String = 'React';
+
+  return (
+    <h1>hello, {name}  World ! </h1>
+  )
+}
+
+export default App;
+
